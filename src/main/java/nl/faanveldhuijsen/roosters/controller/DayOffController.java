02250c29f7b5af473def5c7ccf7b1d92b4f7c102
lfaoanl <12160144+lfaoanl@users.noreply.github.com>
2022-07-06 15:01:47 +0200

    Removed unused imports

--- src/main/java/nl/faanveldhuijsen/roosters/controller/DayOffController.java
+++ src/main/java/nl/faanveldhuijsen/roosters/controller/DayOffController.java
@@ -2,21 +2,14 @@ package nl.faanveldhuijsen.roosters.controller;
 
 import lombok.RequiredArgsConstructor;
 import nl.faanveldhuijsen.roosters.dto.DayOffData;
-import nl.faanveldhuijsen.roosters.dto.DayOffDataSlim;
-import nl.faanveldhuijsen.roosters.dto.UserData;
-import nl.faanveldhuijsen.roosters.dto.mapper.IUserMapper;
-import nl.faanveldhuijsen.roosters.model.User;
 import nl.faanveldhuijsen.roosters.service.DateTimeService;
 import nl.faanveldhuijsen.roosters.service.DayOffService;
-import nl.faanveldhuijsen.roosters.service.UserService;
 import nl.faanveldhuijsen.roosters.utils.DefaultResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @RequiredArgsConstructor
